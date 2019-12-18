45b47fbc1d91c490fa59eebc27553e9f131a25bd
ollie <ollierahman@gmail.com>
2019-12-18 22:00:58 +0000

    Added fish json

--- pkg/gameplay/fishing/fishing.go
+++ pkg/gameplay/fishing/fishing.go
@@ -1,5 +1,9 @@
 package fishing
 
+// Any new attributes need to have a capital letter and a json tag that matches a value in the fish json.
+// You can't sort by name in the json due to weird issues.
+// It makes more sense to sort by a string value of an integer "0", "1"...
+
 type Fish struct {
 	Name  string `json:"name"`
 	Value int    `json:"value"`
